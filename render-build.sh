298ead0ed137da8c537ebb12a5685ff487a1c6f2
Vishal Kokane <88375109+kokane-vishal@users.noreply.github.com>
2024-11-25 16:24:41 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Remove unused packages and configuration files
+apt-get autoremove && apt-get autopurge
+
 # Update package list and install Tesseract OCR
 apt-get update && apt-get install -y tesseract-ocr
 
